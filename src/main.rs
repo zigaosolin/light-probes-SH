289a45f6acbfe30e7df1504b2e5da2856e766356
zigaosolin <ziga.osolin@gmail.com>
2019-11-09 19:29:20 +0100

    Added SH from function

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,11 @@
 mod spherical_harmonics;
 
 use spherical_harmonics::Direction;
-use spherical_harmonics::SH3;
+use spherical_harmonics::SHFuncApproximation;
 
 fn main() {
 	let direction = Direction::new(0f32, 1f32, 0f32);
-	let mut sh3 = SH3::new();
+	let mut sh3 = SHFuncApproximation::new();
 	sh3.from_direction(direction);
     println!("Result is {:?}", sh3);
 }
--- src/spherical_harmonics.rs
+++ src/spherical_harmonics.rs
@@ -1,6 +1,8 @@
+use rand::Rng;
+
 // A direction, represented as floats. The constraint it keeps is
 // that the length is always 1
-#[derive(Debug)]
+#[derive(Debug, Copy, Clone)]
 pub struct Direction {
 	x: f32,
 	y: f32,
@@ -12,18 +14,38 @@ impl Direction {
 		assert!( (x*x + y*y + z*z - 1f32).abs() < 1e-5f32, "Direction is not normalized");
 		Direction {x: x, y: y, z: z }
 	}
+
+	// We use rejection method for generation. Generate in cube, and retry
+	// if we get the point outside the sphere
+	fn generate_random_on_sphere<R>(rng : &mut R) -> Direction
+		where R : Rng {
+
+		loop {
+			let x = rng.gen::<f32>() * 2f32 - 1f32;
+			let y = rng.gen::<f32>() * 2f32 - 1f32;
+			let z = rng.gen::<f32>() * 2f32 - 1f32;
+
+			let r2 = x*x + y*y + z*z;
+			if r2 > 1f32 {
+				continue;
+			}
+
+			let r = r2.sqrt();
+			return Direction {x: x/r, y: y/r, z: z/r};
+		}
+	}
 }
 
-// Spherical harmonic coefficients up to order 3. Represents coefficients only for
-// one channel
+// Spherical harmonic coefficients. Represents coefficients only for
+// one channel. You can represent any function on sphere as this structure
 #[derive(Debug, Clone)]
-pub struct SH3 {
+pub struct SHFuncApproximation {
 	coefficients : Vec<f32> 
 }
 
-impl SH3 {
-	pub fn new() -> SH3 {
-		SH3 { coefficients: vec![0f32; 9]}
+impl SHFuncApproximation {
+	pub fn new() -> SHFuncApproximation {
+		SHFuncApproximation { coefficients: vec![0f32; 9]}
 	}
 
 	pub fn mul_in_place(&mut self, scalar : f32) {
@@ -32,13 +54,13 @@ impl SH3 {
 		}
 	}
 
-	pub fn add_in_place(&mut self, other: SH3) {
+	pub fn add_in_place(&mut self, other: &SHFuncApproximation) {
 		for i in 0..9 {
 			self.coefficients[i] += other.coefficients[i];
 		}
 	}
 
-	pub fn convolution(&self, other : &SH3) -> f32 {
+	pub fn convolution(&self, other : &SHFuncApproximation) -> f32 {
 		let mut result = 0f32;
 		for i in 0..9 {
 			result += self.coefficients[i] * other.coefficients[i];
@@ -76,6 +98,33 @@ impl SH3 {
 		sh[8] = f_tmp_c * f_c1;
 		sh[4] = f_tmp_c * f_s1;
 	}
+
+	pub fn from_function<F, R>(func: F, mut rng: &mut R, count: u32) -> SHFuncApproximation
+		where F : Fn(f32, f32, f32) -> f32, R : Rng {
+
+		let mut approximation = SHFuncApproximation::new();
+		let mut temporary = SHFuncApproximation::new();
+
+		for _i in 0..count {
+			let direction = Direction::generate_random_on_sphere(&mut rng);
+
+			temporary.from_direction(direction);
+
+			let func_value = func(direction.x, direction.y, direction.z);
+			temporary.mul_in_place(func_value);
+
+			approximation.add_in_place(&temporary);
+		}
+
+
+		approximation
+	}
+
+}
+
+#[cfg(tests)]
+mod tests {
+
 }
 
 
